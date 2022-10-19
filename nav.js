39c31e2fcd90a195b594776076971eb5a8526019
Olatunde Abifarin <abifarintunde@gmail.com>
2022-10-19 12:21:10 +0100

    COMMIT FOR PROJECT 5

--- nav.js
+++ nav.js
@@ -1,14 +1,22 @@
-const primaryNav = document.querySelector(".primary-navigation");
-const navToggle = document.querySelector(".mobile-nav-toggle");
+// const primaryNav = document.querySelector(".primary-navigation");
+// const navToggle = document.querySelector(".mobile-nav-toggle");
 
-navToggle.addEventListener("click", () => {
-    const visibility = primaryNav.getAttribute("data-visible")
+// navToggle.addEventListener("click", () => {
+//     const visibility = primaryNav.getAttribute("data-visible")
 
-    if (visibility === "false") {
-        primaryNav.setAttribute("data-visible", true);
-        navToggle.setAttribute("arai-expanded", true);
-    } else if (visibility === "true") {
-        primaryNav.setAttribute("data-visible", false);
-        navToggle.setAttribute("arai-expanded", false);
-    }
-});
+//     if (visibility === "false") {
+//         primaryNav.setAttribute("data-visible", true);
+//         navToggle.setAttribute("arai-expanded", true);
+//     } else if (visibility === "true") {
+//         primaryNav.setAttribute("data-visible", false);
+//         navToggle.setAttribute("arai-expanded", false);
+//     }
+// });
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.primary-navigation');
+
+hamburger.addEventListener('click', () =>{
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
